cc1177aec26e274b0fa302f94d0149b3fc6fe8c4
Jahn <ioannis.kiwe@gmail.com>
2015-01-15 20:37:09 +0100

    public/js/app.js: Refactored app.js code. Now it is shorter and cleaner

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,6 @@
 (function($) {
     var socket = io.connect('http://localhost:8089');
-
-    var sendingUp;
-    var sendingDown;
-    var sendingLeft;
-    var sendingRight;
+    var isSendingCommand;
 
     function sendCommand(command, shouldContinue) {
         setTimeout(function() {
@@ -17,49 +13,15 @@
 
     $("#up, #down, #left, #right").mousedown(function(e) {
         var command = e.target.id;
-        switch (e.target.id) {
-            case "up" :
-                sendingUp = true;
-                sendCommand(command, function() {
-                    return sendingUp;
-                });
-                break;
-            case "down" :
-                sendingDown = true;
-                sendCommand(command, function() {
-                    return sendingDown;
-                });
-                break;
-            case "left" :
-                sendingLeft = true;
-                sendCommand(command, function() {
-                    return sendingLeft;
-                });
-                break;
-            case "right" :
-                sendingRight = true;
-                sendCommand(command, function() {
-                    return sendingRight;
-                });
-                break;
-        }
+        isSendingCommand = true;
+
+        sendCommand(command, function() {
+            return isSendingCommand;
+        });
     });
 
-    $("#up, #down, #left, #right").mouseup(function(e) {
-        switch (e.target.id) {
-            case "up" :
-                sendingUp = false;
-                break;
-            case "down" :
-                sendingDown = false;
-                break;
-            case "left" :
-                sendingLeft = false;
-                break;
-            case "right" :
-                sendingRight = false;
-                break;
-        }
+    $("#up, #down, #left, #right").mouseup(function() {
+        isSendingCommand = false;
     });
 
 })(jQuery);
