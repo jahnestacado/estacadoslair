b2117808b3a244472570b3c05db4a04e036ae200
Jahn <ioannis.kiwe@gmail.com>
2015-01-17 23:28:35 +0100

    Adjusted servo movements to be smaller in order to have smoother controls

--- server.js
+++ server.js
@@ -17,19 +17,22 @@ var min_Y_Axis = 0.07;
 var max_X_Axis = 0.23;
 var min_X_Axis = 0.15;
 
+var SERVO_MOVEMENT_SPEED = 0.002;
+
 //Axises position trackers (initially default position at center)
 var Y_Axis_Value = 0.10;
 var X_Axis_Value = 0.187;
+
+//Set starting position
+piBlaster.setPwm(Y_AXIS_PIN, Y_Axis_Value);
+piBlaster.setPwm(X_AXIS_PIN, X_Axis_Value);
 
 //Served folders
 app.use(express.static('public'));
 
 io.sockets.on('connection', function(socket) {
     console.log("OnConnection ");
-   
-    //Set starting position
-    piBlaster.setPwm(Y_AXIS_PIN, Y_Axis_Value);
-    piBlaster.setPwm(X_AXIS_PIN, X_Axis_Value);
+
 
     socket.on("movement-command", function(command) {
         switch (command) {
@@ -50,34 +53,34 @@ io.sockets.on('connection', function(socket) {
 
 });
 
-function roundTo2Decimals(number) {
-    return Math.round(number * 100) / 100;
+function roundTo3Decimals(number) {
+    return Math.round(number * 1000) / 1000;
 }
 
 function moveServoUp() {
     if (Y_Axis_Value < max_Y_Axis) {
-        Y_Axis_Value += 0.01;
-        piBlaster.setPwm(Y_AXIS_PIN, roundTo2Decimals(Y_Axis_Value));
+        Y_Axis_Value += SERVO_MOVEMENT_SPEED;
+        piBlaster.setPwm(Y_AXIS_PIN, roundTo3Decimals(Y_Axis_Value));
     }
 }
 
 function moveServoDown() {
     if (Y_Axis_Value > min_Y_Axis) {
-        Y_Axis_Value -= 0.01;
-        piBlaster.setPwm(Y_AXIS_PIN, roundTo2Decimals(Y_Axis_Value));
+        Y_Axis_Value -= SERVO_MOVEMENT_SPEED;
+        piBlaster.setPwm(Y_AXIS_PIN, roundTo3Decimals(Y_Axis_Value));
     }
 }
 
 function moveServoLeft() {
     if (X_Axis_Value > min_X_Axis) {
-        X_Axis_Value -= 0.01;
-        piBlaster.setPwm(X_AXIS_PIN, roundTo2Decimals(X_Axis_Value));
+        X_Axis_Value -= SERVO_MOVEMENT_SPEED;
+        piBlaster.setPwm(X_AXIS_PIN, roundTo3Decimals(X_Axis_Value));
     }
 }
 
 function moveServoRight() {
     if (X_Axis_Value < max_X_Axis) {
-        X_Axis_Value += 0.01;
-        piBlaster.setPwm(X_AXIS_PIN, roundTo2Decimals(X_Axis_Value));
+        X_Axis_Value += SERVO_MOVEMENT_SPEED;
+        piBlaster.setPwm(X_AXIS_PIN, roundTo3Decimals(X_Axis_Value));
     }
 }
