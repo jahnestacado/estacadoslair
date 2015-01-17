8f74b06ad0d53d1c0df494833ac227efa2011d6a
Jahn <ioannis.kiwe@gmail.com>
2015-01-17 12:24:16 +0100

    server.js: Changed min-max Y_axis values and round function

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ server.listen(8089);
 
 var Y_AXIS_PIN = 4;
 
-var max_Y_Axis = 2.0;
-var min_Y_Axis = 1.0;
+var max_Y_Axis = 0.14;
+var min_Y_Axis = 0.07;
 
-var Y_Axis_Value = 1.5;
+var Y_Axis_Value = 0.10;
 
-function roundToOneDecimal(number) {
-    return Math.round(number * 10) / 10;
+function roundTo2Decimals(number) {
+    return Math.round(number * 100) / 100;
 }
 
 function moveServoUp() {
     if (Y_Axis_Value < max_Y_Axis) {
-        Y_Axis_Value += 0.1;
+        Y_Axis_Value += 0.01;
         piBlaster.setPwm(Y_AXIS_PIN, roundTo2Decimals(Y_Axis_Value));
     }
 }
 
 function moveServoDown() {
     if (Y_Axis_Value > min_Y_Axis) {
-        Y_Axis_Value -= 0.1;
+        Y_Axis_Value -= 0.01;
         piBlaster.setPwm(Y_AXIS_PIN, roundTo2Decimals(Y_Axis_Value));
     }
 }
